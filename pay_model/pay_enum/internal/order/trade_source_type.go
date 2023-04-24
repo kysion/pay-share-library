62e57c532e497ce9922c9a36750b8c2253fe6a47
0852FeiFeiLin <2778368047@qq.com>
2023-04-24 20:08:13 +0800

    fix: 修复枚举和数据库不统一Bug

--- pay_model/pay_enum/internal/order/trade_source_type.go
+++ pay_model/pay_enum/internal/order/trade_source_type.go
@@ -5,22 +5,22 @@ import "github.com/kysion/base-library/utility/enum"
 type TradeSourceTypeEnum enum.IEnumCode[int]
 
 type tradeSourceType struct {
+	Alipay   TradeSourceTypeEnum
     Weixin   TradeSourceTypeEnum
-    Alipay   TradeSourceTypeEnum
     Douyin   TradeSourceTypeEnum
     Unionpay TradeSourceTypeEnum
 }
 
 var TradeSourceType = tradeSourceType{
-    Weixin:   enum.New[TradeSourceTypeEnum](1, "微信"),
-    Alipay:   enum.New[TradeSourceTypeEnum](2, "支付宝"),
+	Alipay:   enum.New[TradeSourceTypeEnum](1, "支付宝"),
+	Weixin :   enum.New[TradeSourceTypeEnum](2, "微信"),
     Douyin:   enum.New[TradeSourceTypeEnum](4, "抖音"),
     Unionpay: enum.New[TradeSourceTypeEnum](8, "银联"),
 }
 
 func (e tradeSourceType) New(code int, description string) TradeSourceTypeEnum {
-    if (code&TradeSourceType.Weixin.Code()) == TradeSourceType.Weixin.Code() ||
-        (code&TradeSourceType.Alipay.Code()) == TradeSourceType.Alipay.Code() ||
+    if (code&TradeSourceType.Alipay.Code()) == TradeSourceType.Alipay.Code()||
+		(code&TradeSourceType.Weixin.Code()) == TradeSourceType.Weixin.Code() ||
         (code&TradeSourceType.Douyin.Code()) == TradeSourceType.Douyin.Code() ||
         (code&TradeSourceType.Unionpay.Code()) == TradeSourceType.Unionpay.Code() {
         return enum.New[TradeSourceTypeEnum](code, description)
